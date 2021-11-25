4bf8f5bb3efb41c2b67da760c5e6c2b40f8f0581
Bharath-In-D <92077947+Bharath-In-D@users.noreply.github.com>
2021-11-25 18:22:46 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,10 @@
-# using flask_restful
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask import Flask
 
-# creating the flask app
 app = Flask(__name__)
-# creating an API object
-api = Api(app)
-
-class Index(Resource):
-  def index(self):
-    print("Hello Bharath")
-    return jsonify({'message':"Bharath"})
 
 
-# adding the defined resources along with their corresponding urls
-api.add_resource(Hello, '/')
+def hello_world():
+	return 'Hello World'
 
-
-# driver function
 if __name__ == '__main__':
-
-	app.run(debug = True)
+	app.run()
